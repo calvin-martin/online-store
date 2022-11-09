b7c7b7af8536badb9aea615dbb57166a8f9f4108
Calvin Martin <calvin@sliceiq.com>
2022-11-09 13:56:54 +0200

    ORM Config working with one entity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsService } from './models/products.service';
+import { Product } from './models/product.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+    "type": "mysql",
+    "host": "localhost",
+    "port": 3306,
+    "username": "root",
+    "password": "password",
+    "database": "online_store",
+    "entities": [
+        "dist/**/*.entity{.ts,.js}"
+     ],
+    "synchronize": true
+    }),
+    TypeOrmModule.forFeature([Product]),
   ],
   controllers: [AppController, ProductsController],
+  providers: [ ProductsService ],
 })
 export class AppModule {}
