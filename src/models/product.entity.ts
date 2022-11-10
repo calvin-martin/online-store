cc61e66cbac1766df30bf6dd5df8e435c4d3443b
Calvin Martin <calvin@sliceiq.com>
2022-11-10 12:42:25 +0200

    Typeorm - integration fixed (forcibly)

--- src/models/product.entity.ts
+++ src/models/product.entity.ts
@@ -3,17 +3,53 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
 @Entity()
 export class Product {
     @PrimaryGeneratedColumn()
-    id:number;
+    id: number;
 
     @Column()
-    name: string;
-
+    private name: string;
     @Column()
-    description: string;
-
+    private description: string;
     @Column()
-    image: string;
-
+    private image: string;
     @Column()
-    price: number;
+    private price: number;
+
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public setId(value: number) {
+        this.id = value;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(value: string) {
+        this.name = value;
+    }
+
+    public getDescription(): string {
+        return this.description;
+    }
+    public setDescription(value: string) {
+        this.description = value;
+    }
+
+    public getImage(): string {
+        return this.image;
+    }
+    public setImage(value: string) {
+        this.image = value;
+    }
+
+    public getPrice(): number {
+        return this.price;
+    }
+    public setPrice(value: number) {
+        this.price = value;
+    }
+
 }
